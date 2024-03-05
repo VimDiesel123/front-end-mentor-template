e2f86d105868dd9e0a05194418071767f5cf3fa6
VimDiesel123 <Dsmith141996@gmail.com>
2024-03-05 14:00:43 -0500

    Formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-const App = () => 
-<div className="bg-red-500">
- <h1>Hello World!!</h1>
- <span className="font-bold text-[white]">And Tailwind!</span>
-</div>
+const App = () => (
+  <div className="grid bg-red-500">
+    <h1>Hello World!!</h1>
+    <span className="font-bold text-[white]">And Tailwind!</span>
+  </div>
+);
 
 export default App;
